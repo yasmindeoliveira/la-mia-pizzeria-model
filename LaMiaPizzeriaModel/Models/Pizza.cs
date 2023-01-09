3627a2afccf3fc083f7433b5b9331ae7b742b640
yasmindeoliveira <yaz.oliveira01@gmail.com>
2023-01-09 16:16:05 +0100

    completato con foreach

--- LaMiaPizzeriaModel/Models/Pizza.cs
+++ LaMiaPizzeriaModel/Models/Pizza.cs
@@ -1,6 +1,18 @@
 ﻿namespace LaMiaPizzeriaModel.Models
 {
-    public class Post
+    public class Pizza
     {
+        public string nome { get; set; }
+        public string foto { get; set; }
+        public string descrizione { get; set; }
+        public double prezzo { get; set; }
+
+        public Pizza(string nome, string foto, string descrizione, double prezzo)
+        {
+            this.nome = nome;
+            this.foto = foto;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+        }
     }
 }
--- LaMiaPizzeriaModel/Utils/PizzaData.cs
+++ LaMiaPizzeriaModel/Utils/PizzaData.cs
@@ -15,9 +15,9 @@ namespace LaMiaPizzeriaModel.Utils
 
             pizzas = new List<Pizza>()
             {
-                new Pizza ("Margherita", "~/img/Margherita.png", "(Pomodoro, Mozzarella)", 4.50),
-                new Pizza ("Ortolana", "~/img/Ortolana.png", "(Pomodoro, Mozzarella, Verdure Grigliate)", 6.50),
-                new Pizza ("Prosciutto", "~/img/Prosciutto.png", "(Pomodoro, Mozzarella, Prosciutto Cotto)", 5)
+                new Pizza ("Margherita", "img/Margherita.png", "(Pomodoro, Mozzarella)", 4.50),
+                new Pizza ("Ortolana", "img/Ortolana.png", "(Pomodoro, Mozzarella, Verdure Grigliate)", 6.50),
+                new Pizza ("Prosciutto", "img/Prosciutto.png", "(Pomodoro, Mozzarella, Prosciutto Cotto)", 5)
             };
 
             return pizzas; 
